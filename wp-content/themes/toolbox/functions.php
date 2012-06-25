2ec055851d1984bf415ef9f77f89bfaf7d0bbe96
Saki Sato <sakiisato@gmail.com>
2012-06-25 19:35:02 -0400

    added get max char length for excerpt, and get tags by category

--- wp-content/themes/toolbox/functions.php
+++ wp-content/themes/toolbox/functions.php
@@ -123,21 +123,17 @@ add_filter( 'wp_page_menu_args', 'toolbox_page_menu_args' );
 if ( function_exists ('register_sidebar')) { 
     register_sidebar( array(
         'name' => __( 'cat-posts' ),
-        'id' => 'cat-posts',
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => "</aside>",
-        'before_title' => '<h1 class="widget-title">',
-        'after_title' => '</h1>',
+        'id' => 'cat-posts'
     ) );
     
     register_sidebar( array(
         'name' => __( 'cat-children' ),
-        'id' => 'cat-children',
-        'description' => __( 'An optional second sidebar area', 'toolbox' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget' => "</aside>",
-        'before_title' => '<h1 class="widget-title">',
-        'after_title' => '</h1>',
+        'id' => 'cat-children'
+    ) );
+    
+    register_sidebar( array(
+        'name' => __( 'blog' ),
+        'id' => 'blog',
     ) );
 }
 
@@ -343,6 +339,53 @@ function get_first_attachment() {
         return $link;
 }
 
+function the_excerpt_max_charlength($charlength) {
+	$excerpt = get_the_excerpt();
+	$charlength++;
+    
+	if ( mb_strlen( $excerpt ) > $charlength ) {
+		$subex = mb_substr( $excerpt, 0, $charlength - 5 );
+		$exwords = explode( ' ', $subex );
+		$excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+		if ( $excut < 0 ) {
+			echo mb_substr( $subex, 0, $excut );
+		} else {
+			echo $subex;
+		}
+		echo '[...]';
+	} else {
+		echo $excerpt;
+	}
+}
+
+function get_category_tags($args) {
+	global $wpdb;
+	$tags = $wpdb->get_results
+	("
+		SELECT DISTINCT terms2.term_id as tag_id, terms2.name as tag_name, null as tag_link
+		FROM
+			wp_posts as p1
+			LEFT JOIN wp_term_relationships as r1 ON p1.ID = r1.object_ID
+			LEFT JOIN wp_term_taxonomy as t1 ON r1.term_taxonomy_id = t1.term_taxonomy_id
+			LEFT JOIN wp_terms as terms1 ON t1.term_id = terms1.term_id,
+
+			wp_posts as p2
+			LEFT JOIN wp_term_relationships as r2 ON p2.ID = r2.object_ID
+			LEFT JOIN wp_term_taxonomy as t2 ON r2.term_taxonomy_id = t2.term_taxonomy_id
+			LEFT JOIN wp_terms as terms2 ON t2.term_id = terms2.term_id
+		WHERE
+			t1.taxonomy = 'category' AND p1.post_status = 'publish' AND terms1.term_id IN (".$args['categories'].") AND
+			t2.taxonomy = 'post_tag' AND p2.post_status = 'publish'
+			AND p1.ID = p2.ID
+		ORDER by tag_name
+	");
+	$count = 0;
+	foreach ($tags as $tag) {
+		$tags[$count]->tag_link = get_tag_link($tag->tag_id);
+		$count++;
+	}
+	return $tags;
+}
 
 /**
  * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
