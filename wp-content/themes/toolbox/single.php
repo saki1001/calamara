93e4dc53e6378610aef9a46a4b335a866568b364
Saki Sato <sakiisato@gmail.com>
2012-06-26 19:45:30 -0400

    routes to different templates based on category

--- wp-content/themes/toolbox/single.php
+++ wp-content/themes/toolbox/single.php
@@ -8,27 +8,40 @@
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+        <div id="primary">
+            <div id="content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+            <?php
+                // Determine parent cat and current cat
+                $categories = get_the_category();
+                $parent_cat_num = $categories[0]->parent;
+                $current_cat_name = $categories[0]->cat_name;
+                
+                // Declare sidebar variable
+                $sidebar = '';
+                
+                while ( have_posts() ) : the_post();
+                // PORTFOLIO for parent categories New Work and Archives
+                    if ( in_category('new-work') || $parent_cat_num === '10' ) :
+                        $sidebar = 'cat-posts';
+                        get_template_part( 'content', 'portfolio' );
+                // BLOG for parent categories Blog and News
+                    elseif ( in_category('blog') ) :
+                        $sidebar = 'blog';
+                        get_template_part( 'content', 'single' );
+                        // If comments are open or we have at least one comment, load up the comment template
+                        if ( comments_open() || '0' != get_comments_number() ) :
+                            comments_template( '', true );
+                        endif;
+                    else :
+                        get_template_part( 'content', 'page' );
+                    endif;
+                    
+                endwhile; // end of the loop.
+            ?>
+            
+            </div><!-- #content -->
+        </div><!-- #primary -->
 
-				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php get_template_part( 'content', 'single' ); ?>
-
-				<?php toolbox_content_nav( 'nav-below' ); ?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() )
-						comments_template( '', true );
-				?>
-
-			<?php endwhile; // end of the loop. ?>
-
-			</div><!-- #content -->
-		</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
+<?php get_sidebar($sidebar); ?>
 <?php get_footer(); ?>
