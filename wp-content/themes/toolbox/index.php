5ea176b64649d11be8f335846b9c6c2a9cbec0b1
Saki Sato <sakiisato@gmail.com>
2012-06-26 19:44:24 -0400

    cleaned up so it just includes route to content-home, in future should add else statement"

--- wp-content/themes/toolbox/index.php
+++ wp-content/themes/toolbox/index.php
@@ -13,42 +13,11 @@
  */
 
 get_header(); ?>
-
+        
         <div id="primary">
             <div id="content" role="main">
-            <!-- get posts in Home category -->
-            <?php query_posts( 'category_name=home' ); ?>
-            <!-- Start the loop -->
-            <?php if ( have_posts() ) : ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php
-                        /* Include the Post-Format-specific template for the content.
-                         * If you want to overload this in a child theme then include a file
-                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                         */
-                        get_template_part( 'content-home', get_post_format() );
-                        
-                    ?>
-
-                <?php endwhile; ?>
-
-            <?php else : ?>
-
-                <article id="post-0" class="post no-results not-found">
-                    <header class="entry-header">
-                        <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-                    </header><!-- .entry-header -->
-
-                    <div class="entry-content">
-                        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-                        <?php get_search_form(); ?>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-0 -->
-
-            <?php endif; ?>
-            </div><!-- #content -->
-        </div><!-- #primary -->
-
-<?php /*get_sidebar();*/ ?>
+                <?php
+                    get_template_part( 'content-home', get_post_format() );
+                ?>
+                
 <?php get_footer(); ?>
