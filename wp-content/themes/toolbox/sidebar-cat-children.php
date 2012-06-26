28d7e6cc2fe6aea2947e608512ea7ae4c20dbe79
Saki Sato <sakiisato@gmail.com>
2012-06-26 19:49:24 -0400

    now shows current child categories highlighted

--- wp-content/themes/toolbox/sidebar-cat-children.php
+++ wp-content/themes/toolbox/sidebar-cat-children.php
@@ -6,51 +6,30 @@
  * @since Toolbox 0.1
  */
 ?>
+        <?php
+        if ( have_posts() ) :
+            $categories = get_the_category();
+            $parent_cat_num = $categories[0]->parent;
+            $primary_cat_name = $categories[0]->cat_name;
+        ?>
         <div id="secondary" class="widget-area" role="complementary">
-            <?php do_action( 'before_sidebar' ); ?>
-            
-            Sidebar Cat Children
-            
+            <h2>Cat Children</h2>
             <ul>
             <?php
-                // archives id=10
-                $cat = 10;
-                $categories = get_categories('child_of=' . $cat . '&hide_empty=1');
-                // print_r($categories);
-                
-                foreach( $categories as $category ) {
-                    // echo $category->category_nicename;
-                    echo '<li><a href="'.get_category_link($category->cat_ID).'">';
-                    echo $category->cat_name . '</a>';
-                    echo '</li>';
-                }
-                
-                // wp_list_categories('child_of=' . $category->category_parent);
-                
-                // foreach( ( get_the_category() ) as $category ) {
-                //     $the_query = new WP_Query('category_name=' . $category->category_parent);
-                //     print_r($the_query);
-                // }
-            ?>
-            </ul>
+                $child_categories = get_categories('child_of=' . $parent_cat_num . '&hide_empty=1');
+                foreach( $child_categories as $category ) {
+                    $class = '';
+                    if ($primary_cat_name === $category->cat_name) :
+                        // $class = "class='current'";
+                        $class = "style='color: red;'";
+                    endif;
+                ?>
+                    <li <?php echo $class; ?>>
+                        <a href="<?php echo get_category_link($category->cat_ID); ?>"><?php echo $category->cat_name; ?></a>
+                    </li>
             <?php
-            // global $ancestor;
-            // $childcats = get_categories('child_of=' . $cat . '&hide_empty=1');
-            // foreach ($childcats as $childcat) {
-            //   if (cat_is_ancestor_of($ancestor, $childcat->cat_ID) == false){
-            //     echo '<li><h2><a href="'.get_category_link($childcat->cat_ID).'">';
-            //     echo $childcat->cat_name . '</a></h2>';
-            //     echo '<p>'.$childcat->category_description.'</p>';
-            //     echo '</li>';
-            //     $ancestor = $childcat->cat_ID;
-            //   }
-            // }
+                }
             ?>
             </ul>
-            
-            <?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-
-
-            <?php endif; // end sidebar widget area ?>
         </div><!-- #secondary .widget-area -->
+        <?php endif; ?>
