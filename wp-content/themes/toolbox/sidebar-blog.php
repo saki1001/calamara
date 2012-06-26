8d6b641d349da1c3627161622586d2a5ec2e15d8
Saki Sato <sakiisato@gmail.com>
2012-06-26 19:48:04 -0400

    added if else for if have posts, currently doesnt get current tag category

--- wp-content/themes/toolbox/sidebar-blog.php
+++ wp-content/themes/toolbox/sidebar-blog.php
@@ -6,30 +6,37 @@
  * @since Toolbox 0.1
  */
 ?>
+        <?php
+        if ( have_posts() ) :
+            $categories = get_the_category();
+            $cat_num = $categories[0]->cat_ID;
+            $cat_name = $categories[0]->cat_name;
+        ?>
         <div id="secondary" class="widget-area" role="complementary">
-            
-            <h2>Recent Posts</h2>
+            <h1>Blog sidebar</h1>
+            <!-- <h2>Recent Posts</h2>
             <ul>
             <?php
-                // get recent posts from Blog category
-                $args = array( 'category' => '5' );
-                $recent_posts = wp_get_recent_posts($args);
-                foreach( $recent_posts as $recent ){
-                    echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+                // // get recent posts from Blog category
+                // $post_args = array( 'category' => $cat_num );
+                // $recent_posts = wp_get_recent_posts($post_args);
+                // foreach( $recent_posts as $recent ){
+                //     echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+                // }
+            ?>
+            </ul> -->
+            
+            <h2>Filter</h2>
+            <ul>
+                <li><a href="<?php echo get_category_link($cat_num); ?>">View all</a></li>
+            <?php
+                // get tags for posts in Blog category
+                $tag_args = array( 'categories' => $cat_num );
+                $tags = get_category_tags($tag_args);
+                foreach ($tags as $tag) {
+                    echo "<li><a href=\"$tag->tag_link\">$tag->tag_name</a></li>";
                 }
             ?>
             </ul>
-            
-            <h2>Filter</h2>
-            <?php
-            // get tags for posts in Blog category
-                $args = array( 'categories' => '5' );
-                $tags = get_category_tags($args);
-                $content = "<ul>";
-                foreach ($tags as $tag) {
-                	$content .= "<li><a href=\"$tag->tag_link\">$tag->tag_name</a></li>";
-                }
-                $content .= "</ul>";
-                echo $content;
-            ?>
         </div><!-- #secondary .widget-area -->
+        <?php endif; ?>
