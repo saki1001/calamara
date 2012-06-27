1ddb4de491487e9fd96e14397635bbb503189eb2
Saki Sato <sakiisato@gmail.com>
2012-06-27 19:11:36 -0400

    simplified classes and html structure

--- wp-content/themes/toolbox/sidebar.php
+++ wp-content/themes/toolbox/sidebar.php
@@ -6,7 +6,7 @@
  * @since Toolbox 0.1
  */
 ?>
-        <div id="secondary" class="widget-area" role="complementary">
+        <div id="sidebar">
             <h2>Normal Sidebar</h2>
             <?php
                 foreach( ( get_the_category() ) as $category ) {
@@ -21,10 +21,4 @@
             }
             ?>
             </ul>
-        </div><!-- #secondary .widget-area -->
-
-        <?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-        <div id="tertiary" class="widget-area" role="complementary">
-            <?php dynamic_sidebar( 'sidebar-2' ); ?>
-        </div><!-- #tertiary .widget-area -->
-        <?php endif; ?>
+        </div><!-- #sidebar -->
