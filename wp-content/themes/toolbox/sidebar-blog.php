52b00124640c877ab016b10dfcea14e56274676a
Saki Sato <sakiisato@gmail.com>
2012-06-27 19:07:25 -0400

    got it to highlight current tag or if viewing all

--- wp-content/themes/toolbox/sidebar-blog.php
+++ wp-content/themes/toolbox/sidebar-blog.php
@@ -9,34 +9,46 @@
         <?php
         if ( have_posts() ) :
             $categories = get_the_category();
-            $cat_num = $categories[0]->cat_ID;
-            $cat_name = $categories[0]->cat_name;
-        ?>
-        <div id="secondary" class="widget-area" role="complementary">
-            <h1>Blog sidebar</h1>
-            <!-- <h2>Recent Posts</h2>
-            <ul>
-            <?php
-                // // get recent posts from Blog category
-                // $post_args = array( 'category' => $cat_num );
-                // $recent_posts = wp_get_recent_posts($post_args);
-                // foreach( $recent_posts as $recent ){
-                //     echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-                // }
-            ?>
-            </ul> -->
+            $current_cat_id = $categories[0]->term_id;
             
+            $current_post_id = '';
+            $current_tag_title = '';
+            $class = '';
+            
+            // if not single, put current class on View All
+            if (is_single($post) === true) :
+                $current_post_id = $post->ID;
+            elseif (is_tag() === true) :
+                $current_tag_title = single_tag_title("", false);
+            elseif (is_single($post) === false) :
+                // $class = "class='current'";
+                $class = "style='color: red;'";
+            endif;
+        ?>
+        <div id="sidebar">
             <h2>Filter</h2>
             <ul>
-                <li><a href="<?php echo get_category_link($cat_num); ?>">View all</a></li>
+                <li <?php echo $class; ?>>
+                    <a href="<?php echo get_category_link($current_cat_id); ?>">View all</a>
+                </li>
             <?php
-                // get tags for posts in Blog category
-                $tag_args = array( 'categories' => $cat_num );
+                // get tags for posts in current category
+                $tag_args = array( 'categories' => $current_cat_id );
                 $tags = get_category_tags($tag_args);
                 foreach ($tags as $tag) {
-                    echo "<li><a href=\"$tag->tag_link\">$tag->tag_name</a></li>";
+                    $class = '';
+                    $title = $tag->tag_name;
+                    if ($current_tag_title === $title) :
+                        // $class = "class='current'";
+                        $class = "style='color: red;'";
+                    endif;
+            ?>
+                    <li <?php echo $class; ?>>
+                        <a href="<?php echo $tag->tag_link; ?>"><?php echo $tag->tag_name; ?></a>
+                    </li>
+            <?php
                 }
             ?>
             </ul>
-        </div><!-- #secondary .widget-area -->
+        </div><!-- #sidebar -->
         <?php endif; ?>
