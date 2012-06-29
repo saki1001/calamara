198704e79669680b2c4a8bbd9cf4f7722234f086
Saki Sato <sakiisato@gmail.com>
2012-06-29 17:17:04 -0400

    removed primary class, cleaned up html

--- wp-content/themes/toolbox/content-page.php
+++ wp-content/themes/toolbox/content-page.php
@@ -8,24 +8,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
-    </header><!-- .entry-header -->
-
     <div class="entry-content">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
         <?php
             // get text
             the_content();
             
-            // get attached image
-            $image = get_first_attachment();
-            
-            if($image != '') {
-                echo "<img src='" . $image . "' width='350' />";
-            } else {
-                // do nothing
-            }
         ?>
         <?php edit_post_link( __( 'Edit', 'toolbox' ), '<span class="edit-link">', '</span>' ); ?>
     </div><!-- .entry-content -->
+    
+    <?php
+        // get attached image
+        $image = get_first_attachment();
+        $imageUrl = '';
+        
+        // insert image
+        if($image != '') {
+    ?>
+        <figure>
+            <img src="<?php echo $image; ?>" width="350" />
+        </figure>
+    <?php
+        } else {
+            // do nothing
+        }
+    ?>
+    </figure>
+    
 </article><!-- #post-<?php the_ID(); ?> -->
