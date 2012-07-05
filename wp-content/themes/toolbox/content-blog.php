660ec89babc4a0a60027ec7fffe2aa096bfc24cd
Saki Sato <sakiisato@gmail.com>
2012-07-05 18:23:47 -0400

    cleaned up and arranged elements for post with and without image thumbs

--- wp-content/themes/toolbox/content-blog.php
+++ wp-content/themes/toolbox/content-blog.php
@@ -3,37 +3,55 @@
  * @package Toolbox
  */
 ?>
+<!-- WITH AND WITHOUT THUMBNAIL TEMPLATES -->
+<?php
+    $images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
+    if ( $images ) :
+        $total_images = count( $images );
+        $image = array_shift( $images );
+        $image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
+        $image_img_tag_url = wp_get_attachment_image_src( $image->ID, 'thumbnail' );
+        
+        $thumbClass = "thumb";
+        $maxExcerptLength = "150";
+    else :
+        $image_img_tag = "";
+        
+        $thumbClass = "text";
+        $maxExcerptLength = "125";
+    endif;
+?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" class="<?php echo $thumbClass; ?>">
+        
     <?php if ( 'post' == get_post_type() ) : ?>
-	<div class="entry-meta">
-		<?php echo date('M d', strtotime(get_the_date())); ?>
-	</div><!-- .entry-meta -->
-	<?php endif; ?>
-	
-	<h3>
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-	</h3><!-- h3 -->
-
-	<div class="entry-content">
-        <?php
-            $images = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'attachment', 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC' ) );
-            if ( $images ) :
-                $total_images = count( $images );
-                $image = array_shift( $images );
-                $image_img_tag = wp_get_attachment_image( $image->ID, 'thumbnail' );
-        ?>
-            <!-- WITH THUMBNAIL -->
-            <?php the_excerpt_max_charlength(100); ?>
-            <figure class="gallery-thumb">
-                <a href="<?php the_permalink(); ?>">
-                    <?php echo $image_img_tag; ?>
+        
+        <div class="entry-meta">
+            <?php echo date('m. d', strtotime(get_the_date())); ?>
+        </div>
+        
+    <?php endif; ?>
+    
+    <div class="entry">
+        
+            <h3 class="entry-title">
+                <a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
+                    <?php the_title(); ?>
                 </a>
-            </figure><!-- .gallery-thumb -->
-        <?php else : ?>
-            <!-- WITHOUT THUMBNAIL -->
-            <?php the_excerpt_max_charlength(200); ?>
-        <?php endif; ?>
-	</div><!-- .entry-content -->
-
+            </h3>
+        
+            <div class="entry-content">
+                <?php the_excerpt_max_charlength($maxExcerptLength); ?>
+            </div>
+        
+    </div>
+    
+    <?php if ($image_img_tag != "" ) : ?>
+        
+        <figure style="background: url('<?php echo $image_img_tag_url[0]; ?>') no-repeat center center;">
+            <!-- background image -->
+        </figure>
+        
+    <?php endif; ?>
+    
 </article><!-- #post-<?php the_ID(); ?> -->
