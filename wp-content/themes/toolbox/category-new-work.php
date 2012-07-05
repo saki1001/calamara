d0f33647eecf8766f1585f1c15540f548b9e329e
Saki Sato <sakiisato@gmail.com>
2012-07-05 18:22:27 -0400

    removed primary container, cleaned up, moved h2 out of main container, includes content-not-found.php

--- wp-content/themes/toolbox/category-new-work.php
+++ wp-content/themes/toolbox/category-new-work.php
@@ -7,58 +7,31 @@
  */
 
 get_header(); ?>
-
-        <section id="primary">
-            <div id="content" role="main">
-
-            <?php if ( have_posts() ) : ?>
-
-                <header class="page-header">
-                    <h1 class="page-title">
-                    New Work: 
-                    <?php
-                        printf( single_cat_title( '', false ) );
-                    ?></h1>
-
-                    <?php
-                        $category_description = category_description();
-                        if ( ! empty( $category_description ) )
-                            echo apply_filters( 'category_archive_meta', '<div class="category-archive-meta">' . $category_description . '</div>' );
-                    ?>
-                </header>
-
-
-                <?php /* Start the Loop */ ?>
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php
-                        /* Include the Post-Format-specific template for the content.
-                         * If you want to overload this in a child theme then include a file
-                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                         */
-                        get_template_part( 'content-thumbs', get_post_format() );
-                    ?>
-
-                <?php endwhile; ?>
-
-
-            <?php else : ?>
-
-                <article id="post-0" class="post no-results not-found">
-                    <header class="entry-header">
-                        <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-                    </header><!-- .entry-header -->
-
-                    <div class="entry-content">
-                        <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-                        <?php get_search_form(); ?>
-                    </div><!-- .entry-content -->
-                </article><!-- #post-0 -->
-
-            <?php endif; ?>
-
-            </div><!-- #content -->
-        </section><!-- #primary -->
-
+    
+    <h2 class="page-title">
+        <?php printf( single_cat_title( '', false ) ); ?>
+    </h2>
+    
+    <section id="content" role="main">
+        
+        <?php if ( have_posts() ) : ?>
+            
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                
+                <?php
+                    // Thumbnail Template
+                    get_template_part( 'content-thumbs', get_post_format() );
+                ?>
+                
+            <?php endwhile; ?>
+            
+        <?php else :
+            // Content Not Found Template
+            include('content-not-found.php');
+        endif; ?>
+        
+    </section>
+        
 <?php get_sidebar('cat-posts'); ?>
 <?php get_footer(); ?>
