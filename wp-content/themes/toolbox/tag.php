b01b469753a84b05f1c6ebb42651b457f252cfce
Saki Sato <sakiisato@gmail.com>
2012-07-05 18:24:34 -0400

    if in news or blog category, is identical to those category pages, otherwise defaults to content.php template

--- wp-content/themes/toolbox/tag.php
+++ wp-content/themes/toolbox/tag.php
@@ -8,59 +8,46 @@
 
 get_header(); ?>
 
-		<section id="primary">
-			<div id="content" role="main">
+<?php
+    // BLOG for parent categories Blog and News
+    if ( in_category('blog') || in_category('news') ) :
+       $contentClass = "class='blog-list'";
+       $postFormat = "content-blog";
+    else :
+        $mainClass = "";
+        $postFormat = "content";
+    endif;
+?>
 
-			<?php if ( have_posts() ) : ?>
-
-				<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Tag Archives: %s', 'toolbox' ), '<span>' . single_tag_title( '', false ) . '</span>' );
-					?></h1>
-
-					<?php
-						$tag_description = tag_description();
-						if ( ! empty( $tag_description ) )
-							echo apply_filters( 'tag_archive_meta', '<div class="tag-archive-meta">' . $tag_description . '</div>' );
-					?>
-				</header>
-
-				<?php rewind_posts(); ?>
-
-				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php toolbox_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
+<section id="content" <?php echo $contentClass; ?> role="main">
+    
+    <?php if ( have_posts() ) : ?>
+        
+            <h2 class="page-title">
+                <?php
+                    // printf( __( 'Tag Archives: %s', 'toolbox' ), '<span>' . single_tag_title( '', false ) . '</span>' );
+                    $parent_category = get_the_category();
+                    echo $parent_category[0]->cat_name . ": " . single_tag_title( '', false )   ;
+                ?>
+            </h2>
+            
+        <?php rewind_posts(); ?>
+            
+            <?php
+                while ( have_posts() ) : the_post(); 
+                
+                    get_template_part( $postFormat, get_post_format() );
+                
+                endwhile;
+            ?>
+        
+    <?php else :
+        
+        include('content-not-found.php');
+        
+    endif; ?>
+    
+</section>
 
 <?php get_sidebar('blog'); ?>
 <?php get_footer(); ?>
