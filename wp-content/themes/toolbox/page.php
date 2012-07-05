922f24917b6f9210a9ec693dd3d8e3bc6369d8c5
Saki Sato <sakiisato@gmail.com>
2012-07-05 18:30:44 -0400

    removed primary container

--- wp-content/themes/toolbox/page.php
+++ wp-content/themes/toolbox/page.php
@@ -12,18 +12,15 @@
  */
 
 get_header(); ?>
-
-        <div id="primary">
-            <div id="content" role="main">
-
-                <?php while ( have_posts() ) : the_post(); ?>
-
-                    <?php get_template_part( 'content', 'page' ); ?>
-
-                <?php endwhile; // end of the loop. ?>
-
-            </div><!-- #content -->
-        </div><!-- #primary -->
-
-<?php /*get_sidebar();*/ ?>
+        
+        <div id="content" role="main">
+            
+            <?php while ( have_posts() ) : the_post(); ?>
+                
+                <?php get_template_part( 'content', 'page' ); ?>
+                
+            <?php endwhile; // end of the loop. ?>
+            
+        </div><!-- #content -->
+        
 <?php get_footer(); ?>
