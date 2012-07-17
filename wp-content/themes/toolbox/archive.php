58dff563529e91624e5b2747ebc3b52b89dd2e80
Saki Sato <sakiisato@gmail.com>
2012-07-17 16:25:30 -0400

    standardized html elements, removed unwanted elements

--- wp-content/themes/toolbox/archive.php
+++ wp-content/themes/toolbox/archive.php
@@ -12,64 +12,47 @@
  */
 
 get_header(); ?>
+    
+    <?php if ( have_posts() ) : ?>
+        
+        <h2 class="page-title">
+            <?php
+                if ( is_day() ) :
+                    printf( __( 'Daily Archives: %s', 'toolbox' ), '<span>' . get_the_date() . '</span>' );
+                elseif ( is_month() ) :
+                    printf( __( 'Monthly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+                elseif ( is_year() ) :
+                    printf( __( 'Yearly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+                else :
+                    _e( 'Archives', 'toolbox' );
+                endif;
+            ?>
+        </h2>
+        
+        <section id="content" role="main">
+            
+            <?php rewind_posts(); ?>
+            
+            <?php 
+                while ( have_posts() ) : the_post();
+                    
+                    if ( in_category('blog') || in_category('news') ) :
+                        // List Excerpts Template
+                        get_template_part( 'content-blog-list', get_post_format() );
+                    else :
+                        // don't show'
+                    endif;
+                    
+                endwhile;
+            ?>
+            
+        </section>
+        
+    <?php else :
+        // Content Not Found Template
+        include('content-not-found.php');
+        
+    endif; ?>
 
-		<section id="primary">
-			<div id="content" role="main">
-
-			<?php if ( have_posts() ) : ?>
-
-				<header class="page-header">
-					<h1 class="page-title">
-						<?php
-							if ( is_day() ) :
-								printf( __( 'Daily Archives: %s', 'toolbox' ), '<span>' . get_the_date() . '</span>' );
-							elseif ( is_month() ) :
-								printf( __( 'Monthly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-							elseif ( is_year() ) :
-								printf( __( 'Yearly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-							else :
-								_e( 'Archives', 'toolbox' );
-							endif;
-						?>
-					</h1>
-				</header>
-
-				<?php rewind_posts(); ?>
-
-				<?php toolbox_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php toolbox_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
-
-			</div><!-- #content -->
-		</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
+<?php get_sidebar('blog'); ?>
 <?php get_footer(); ?>
