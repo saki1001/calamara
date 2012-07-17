9f58008459d313fda33a25ac01adade6d4da4133
Saki Sato <sakiisato@gmail.com>
2012-07-17 16:26:07 -0400

    added section wrapper since it is not included when referenced in templtes

--- wp-content/themes/toolbox/content-not-found.php
+++ wp-content/themes/toolbox/content-not-found.php
@@ -6,7 +6,7 @@
  * @since Toolbox 1.0
  */
 ?>
-    
+<section id="content" role="main">
     <article id="post-0" class="post no-results not-found">
         <header class="entry-header">
             <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
@@ -14,6 +14,6 @@
         
         <div class="entry-content">
             <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-            <?php get_search_form(); ?>
         </div><!-- .entry-content -->
     </article><!-- #post-0 -->
+</section>
