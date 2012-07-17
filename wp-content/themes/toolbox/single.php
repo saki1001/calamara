cd795b45f88ca91fa22a93aa60ff79a3bc47ba93
Saki Sato <sakiisato@gmail.com>
2012-07-17 16:27:48 -0400

    standardized html elements, removed unwanted stuff from toolbox

--- wp-content/themes/toolbox/single.php
+++ wp-content/themes/toolbox/single.php
@@ -7,47 +7,48 @@
  */
 
 get_header(); ?>
-
-<section id="content" role="main">
     
     <?php if ( have_posts() ) : ?>
         
-        <?php
-            // Determine parent cat and current cat
-            $categories = get_the_category();
-            $parent_cat_num = $categories[0]->parent;
-            $current_cat_name = $categories[0]->cat_name;
+        <section id="content" role="main">
             
-            // Declare sidebar variable
-            $sidebar = '';
-            
-            while ( have_posts() ) : the_post();
-            // PORTFOLIO for parent categories New Work and Archives
-                if ( in_category('new-work') || $parent_cat_num === '10' ) :
-                    $sidebar = 'cat-posts';
-                    get_template_part( 'content', 'portfolio-single' );
-            // BLOG for parent categories Blog and News
-                elseif ( in_category('blog') ) :
-                    $sidebar = 'blog';
-                    get_template_part( 'content', 'single' );
-                    // // If comments are open or we have at least one comment, load up the comment template
-                    // if ( comments_open() || '0' != get_comments_number() ) :
-                    //     comments_template( '', true );
-                    // endif;
-                else :
-                    get_template_part( 'content', 'page' );
-                endif;
+            <?php
+                // Determine parent cat and current cat
+                $categories = get_the_category();
+                $parent_cat_num = $categories[0]->parent;
+                $current_cat_name = $categories[0]->cat_name;
                 
-            endwhile; // end of the loop.
-        ?>
-    
+                // Declare sidebar variable
+                $sidebar = '';
+                
+                while ( have_posts() ) : the_post();
+                // PORTFOLIO for parent categories New Work and Archives
+                    if ( in_category('new-work') || $parent_cat_num === '10' ) :
+                        $sidebar = 'cat-posts';
+                        get_template_part( 'content', 'portfolio-single' );
+                        
+                // BLOG for parent categories Blog and News
+                    elseif ( in_category('blog') || in_category('news') ) :
+                        $sidebar = 'blog';
+                        get_template_part( 'content', 'blog-single' );
+                        // // If comments are open or we have at least one comment, load up the comment template
+                        // if ( comments_open() || '0' != get_comments_number() ) :
+                        //     comments_template( '', true );
+                        // endif;
+                        
+                    else :
+                        get_template_part( 'content', 'page' );
+                    endif;
+                
+                endwhile; // end of the loop.
+            ?>
+        </section>
+        
     <?php else :
         
         include('content-not-found.php');
         
     endif; ?>
     
-</section>
-
 <?php get_sidebar($sidebar); ?>
 <?php get_footer(); ?>
