268192e814f5fd76a1f79285d45becd0efb46a3d
Saki Sato <sakiisato@gmail.com>
2012-07-18 19:07:12 -0400

    added cat name to view all link

--- wp-content/themes/toolbox/sidebar-cat-children.php
+++ wp-content/themes/toolbox/sidebar-cat-children.php
@@ -17,19 +17,23 @@
         
         // For Child Categories
         if( $cat_obj->parent ) :
-            $parent_cat_num = $cat_obj->parent;
+            $parent_cat_id = $cat_obj->parent;
+            $parent_cat = get_category($parent_cat_id);
+            $parent_cat_name = $parent_cat->cat_name;
+            
         // For the Parent Category
         else :
-            $parent_cat_num =  $cat_obj->term_id;
+            $parent_cat_id =  $cat_obj->term_id;
+            $parent_cat_name = $cat_obj->name;
             $class = "class='current'";
         endif;
 ?>
         <div id="sidebar">
             <ul>
-                <li <?php echo $class; ?>><a href="<?php echo get_category_link($parent_cat_num); ?>">View All</a></li>
+                <li <?php echo $class; ?>><a href="<?php echo get_category_link($parent_cat_id); ?>">All <?php echo $parent_cat_name; ?></a></li>
                 
                 <?php
-                    $child_categories = get_categories('child_of=' . $parent_cat_num . '&hide_empty=1');
+                    $child_categories = get_categories('child_of=' . $parent_cat_id . '&hide_empty=1');
                     foreach( $child_categories as $category ) {
                         $class = '';
                         if ($primary_cat_name === $category->cat_name) :
