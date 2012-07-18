0f9e9549a80d9f207311c05bb6a0a9ead3671a60
Saki Sato <sakiisato@gmail.com>
2012-07-18 19:27:05 -0400

    implemented Slideshow plugin instead of wp_cycle();

--- wp-content/themes/toolbox/content-home.php
+++ wp-content/themes/toolbox/content-home.php
@@ -9,8 +9,8 @@
 <div id="slideshow" class="<?php post_class(); ?>">
     
     <?php 
-        // do_action('slideshow_deploy', '17');
-        wp_cycle();
+        do_action('slideshow_deploy', '17');
+        // wp_cycle();
     ?>
     
 </div><!-- #slideshow -->
