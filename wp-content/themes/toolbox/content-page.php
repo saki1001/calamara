6264c3b6037064f305282313e111392f5eb24bd0
Saki Sato <sakiisato@gmail.com>
2012-07-18 19:26:00 -0400

    cleaned up how i got attachments and now support multiple attachment with new foreach loop

--- wp-content/themes/toolbox/content-page.php
+++ wp-content/themes/toolbox/content-page.php
@@ -14,36 +14,46 @@
     </h2>
     
     <?php
-        // get attached image
-        $image = get_first_attachment();
-        $noImageClass = '';
+        // Define args to get attachments
+        $args = array(
+            'post_parent' => $post->ID,
+            'post_type' => 'attachment',
+            'post_mime_type' => 'image',
+            'orderby' => 'menu_order',
+            'order' => 'ASC'
+        );
         
-        if($image === '') {
+        // Get image attachments
+        $attachments = get_posts( $args );
+        
+        if ( $attachments ) :
+            $noImageClass = '';
+        else :
             $noImageClass = 'no-image';
-        }
+        endif;
     ?>
     
     <div class="entry-content <?php echo $noImageClass; ?>">
-        <?php
-            // get text
-            the_content();
-            
-        ?>
+        <?php the_content(); ?>
     </div>
     
     <?php
-        // insert image
-        if($image != '') {
-    ?>
-        <figure>
-            <img src="<?php echo $image; ?>" width="350" />
-        </figure>
-    <?php
-        } else {
+        // Insert images as background-image (to show rounded corners)
+        // And as a regular image element (which is hidden)
+        if ( $attachments ) :
+            foreach ( $attachments as $attachment ) {
+                
+                $image = wp_get_attachment_image( $attachment->ID, 'page' );
+                $imageUrl = wp_get_attachment_image_src( $attachment->ID, 'page' );
+                
+                echo '<figure style="background: url(' . $imageUrl[0] . ') no-repeat center center;">';
+                echo $image;
+                echo '</figure>';
+              }
+        else :
             // do nothing
-        }
+        endif;
     ?>
-    </figure>
     
 </article><!-- #post-<?php the_ID(); ?> -->
 
