49beae6560b888cb6a048b69605d26e41fc68abb
Saki Sato <sakiisato@gmail.com>
2012-08-01 21:44:12 -0400

    standardized archive page, and only shows blog or news posts, all others hidden, also uses new archive sidebar

--- wp-content/themes/toolbox/archive.php
+++ wp-content/themes/toolbox/archive.php
@@ -15,35 +15,44 @@ get_header(); ?>
     
     <?php if ( have_posts() ) : ?>
         
-        <h2 class="page-title">
-            <?php
-                if ( is_day() ) :
-                    printf( __( 'Daily Archives: %s', 'toolbox' ), '<span>' . get_the_date() . '</span>' );
-                elseif ( is_month() ) :
-                    printf( __( 'Monthly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-                elseif ( is_year() ) :
-                    printf( __( 'Yearly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-                else :
-                    _e( 'Archives', 'toolbox' );
-                endif;
-            ?>
-        </h2>
-        
-        <section id="content" role="main">
+        <section id="content" class="blog-list" role="main">
+            <h2 class="page-title">
+                <?php
+                    if ( is_day() ) :
+                        printf( __( 'Daily Archives: %s', 'toolbox' ), '<span>' . get_the_date() . '</span>' );
+                    elseif ( is_month() ) :
+                        printf( __( 'Monthly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+                    elseif ( is_year() ) :
+                        printf( __( 'Yearly Archives: %s', 'toolbox' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+                    else :
+                        _e( 'Archives', 'toolbox' );
+                    endif;
+                ?>
+            </h2>
             
             <?php rewind_posts(); ?>
             
-            <?php 
+            <?php
+                
+                $count = 0;
+                
                 while ( have_posts() ) : the_post();
                     
                     if ( in_category('blog') || in_category('news') ) :
                         // List Excerpts Template
                         get_template_part( 'content-blog-list', get_post_format() );
+                        
+                        $count++;
                     else :
-                        // don't show'
+                        // don't show
                     endif;
                     
                 endwhile;
+                
+                if ($count === 0) :
+                    echo '<p class="not-found">Sorry, no results found.</p>';
+                endif;
+            
             ?>
             
         </section>
@@ -54,5 +63,5 @@ get_header(); ?>
         
     endif; ?>
 
-<?php get_sidebar('blog'); ?>
+<?php get_sidebar('archive'); ?>
 <?php get_footer(); ?>
