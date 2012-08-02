93d151b39011a1b381d265d6d48774934a203f6b
Saki Sato <sakiisato@gmail.com>
2012-08-01 21:56:09 -0400

    simplified text

--- wp-content/themes/toolbox/content-not-found.php
+++ wp-content/themes/toolbox/content-not-found.php
@@ -7,13 +7,7 @@
  */
 ?>
 <section id="content" role="main">
-    <article id="post-0" class="post no-results not-found">
-        <header class="entry-header">
-            <h1 class="entry-title"><?php _e( 'Nothing Found', 'toolbox' ); ?></h1>
-        </header><!-- .entry-header -->
-        
-        <div class="entry-content">
-            <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'toolbox' ); ?></p>
-        </div><!-- .entry-content -->
-    </article><!-- #post-0 -->
+    
+    <p class="not-found">Sorry, we can't find what you're looking for.</p>
+    
 </section>
