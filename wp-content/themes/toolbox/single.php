1637e6aef9a15b5751e000b0e88ead9c4acd9c7e
Saki Sato <sakiisato@gmail.com>
2012-08-01 21:54:01 -0400

    made simpler, now routes directly to templates instead of using a varibale to determine which template to route to, this is because I moved the <section> tag into these templates

--- wp-content/themes/toolbox/single.php
+++ wp-content/themes/toolbox/single.php
@@ -8,44 +8,37 @@
 
 get_header(); ?>
     
-    <?php if ( have_posts() ) : ?>
+    <?php
+        // Determine parent cat and current cat
+        $categories = get_the_category();
+        $parent_cat_num = $categories[0]->parent;
         
-        <section id="content" role="main">
-            
-            <?php
-                // Determine parent cat and current cat
-                $categories = get_the_category();
-                $parent_cat_num = $categories[0]->parent;
-                $current_cat_name = $categories[0]->cat_name;
+        // Declare variables
+        $sidebar = '';
+        
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+            // PORTFOLIO for parent categories New Work and Archives
+                if ( in_category('new-work') || $parent_cat_num === '10' ) :
+                    $sidebar = 'cat-posts';
+                    get_template_part('content', 'portfolio-single' );
+                    
+            // BLOG for parent categories Blog and News
+                elseif ( in_category('blog') || in_category('news') ) :
+                    $sidebar = 'blog';
+                    get_template_part('content', 'blog-single' );
+                    
+                else :
+                    // Standard Template
+                    get_template_part('content', 'page' );
+                    
+                endif;
                 
-                // Declare sidebar variable
-                $sidebar = '';
-                
-                while ( have_posts() ) : the_post();
-                // PORTFOLIO for parent categories New Work and Archives
-                    if ( in_category('new-work') || $parent_cat_num === '10' ) :
-                        $sidebar = 'cat-posts';
-                        get_template_part( 'content', 'portfolio-single' );
-                        
-                // BLOG for parent categories Blog and News
-                    elseif ( in_category('blog') || in_category('news') ) :
-                        $sidebar = 'blog';
-                        get_template_part( 'content', 'blog-single' );
-                        // // If comments are open or we have at least one comment, load up the comment template
-                        // if ( comments_open() || '0' != get_comments_number() ) :
-                        //     comments_template( '', true );
-                        // endif;
-                        
-                    else :
-                        get_template_part( 'content', 'page' );
-                    endif;
-                
-                endwhile; // end of the loop.
-            ?>
-        </section>
+            endwhile; // end of the loop.
+    ?>
         
     <?php else :
-        
+        // Content Not Found Template
         include('content-not-found.php');
         
     endif; ?>
