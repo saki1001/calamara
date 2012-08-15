80f1f1f6676d099f187061fc088081ad83259272
Saki Sato <sakiisato@gmail.com>
2012-08-15 16:39:39 -0400

    moved slideshow container into slideshow plugin file

--- wp-content/themes/toolbox/content-home.php
+++ wp-content/themes/toolbox/content-home.php
@@ -4,11 +4,7 @@
  */
 ?>
 
-
-<div class="border-radius"></div>
-<div id="slideshow">
-    <?php
-        // Slideshow set in Home page
-        the_content();
-    ?>
-</div>
+<?php
+    // Slideshow set in Home page
+    the_content();
+?>
