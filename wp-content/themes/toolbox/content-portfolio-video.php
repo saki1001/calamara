f3f1f2588ebfc24abf02c952471016243b783a13
Saki Sato <sakiisato@gmail.com>
2012-08-17 19:16:26 -0400

    video template that parses out iframe from text

---
+++ wp-content/themes/toolbox/content-portfolio-video.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying posts in the Gallery Post Format on index and archive pages
+ *
+ * Learn more: http://codex.wordpress.org/Post_Formats
+ *
+ * @package Toolbox
+ * @since Toolbox 1.0
+ */
+?>
+<section id="content" class="portfolio-single show-images" role="main">
+    <h2 class="page-title">
+        <?php
+            the_title();
+        ?>
+    </h2>
+    
+    <div class="toggle-link">
+        <a href="#" class="text-link">View Text</a>
+        <a href="#" class="image-link">View Video</a>
+    </div>
+    
+    <div id="media">
+        <?php
+            include('php/simple_html_dom.php');
+            
+            // Create a DOM object
+            $html = new simple_html_dom();
+            
+            // Load HTML from a string
+            $html->load(get_the_content());
+            
+            // Find all iframes
+            $iframe = $html->find('iframe');
+            foreach( $iframe as $video) {
+               // Insert iframe
+               echo $video;
+               
+               // Remove iframe after inserting 
+               $video->outertext = '';
+           }
+        ?>
+    </div>
+    
+    <div id="text" class="text-container">
+        <?php
+            echo $html;
+        ?>
+    </div>
+</section>
