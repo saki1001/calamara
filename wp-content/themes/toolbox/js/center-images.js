09e84e638607f2d6387ed821d6ad6dab1bee855e
Saki Sato <sakiisato@gmail.com>
2012-08-17 19:16:44 -0400

    script to center portfolio images

---
+++ wp-content/themes/toolbox/js/center-images.js
@@ -0,0 +1,19 @@
+var $j = jQuery.noConflict();
+
+$j(document).ready(function() {
+
+    // Set Image Width and Margin
+    // for centering images in portfolio pages
+    $j('#media .image-container figure').each(function(i) {
+        
+        var width = $j(this).children('img').width();
+        var height = $j(this).children('img').height();
+        var marginTop = (500 - height)/2 + 'px';
+        
+        $j(this).css({
+            'width': width,
+            'margin': marginTop + ' auto'
+        });
+    });
+
+});
