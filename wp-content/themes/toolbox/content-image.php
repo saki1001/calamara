2bdbed71d8e5d908df287799ca4528b811eefb49
Saki Sato <sakiisato@gmail.com>
2012-08-28 13:42:09 -0400

    put default styles in

--- wp-content/themes/toolbox/content-image.php
+++ wp-content/themes/toolbox/content-image.php
@@ -9,21 +9,18 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-	</header><!-- .entry-header -->
+<section id="content" class="blog-single" role="main">
+    <h2 class="page-title">
+        <?php
+            the_title();
+        ?>
+    </h2>
 
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php toolbox_posted_on(); ?>
-		<?php if ( comments_open() || ( '0' != get_comments_number() && ! comments_open() ) ) : ?>
-		<span class="sep"> | </span>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'toolbox' ), __( '1 Comment', 'toolbox' ), __( '% Comments', 'toolbox' ) ); ?></span>
-		<?php endif; ?>
-	</footer><!-- #entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    
+        <div id="text" class="text-container">
+            <?php the_content(); ?>
+        </div>
+    
+    </article><!-- #post-<?php the_ID(); ?> -->
+</section>
