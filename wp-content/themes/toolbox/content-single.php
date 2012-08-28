047ed711434a3e52714172068cf14b56db159d60
Saki Sato <sakiisato@gmail.com>
2012-08-28 13:42:17 -0400

    put default styles in

--- wp-content/themes/toolbox/content-single.php
+++ wp-content/themes/toolbox/content-single.php
@@ -3,47 +3,38 @@
  * @package Toolbox
  */
 ?>
+<section id="content" class="blog-single" role="main">
+    <h2 class="page-title">
+        <?php
+            the_title();
+        ?>
+    </h2>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'toolbox' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'toolbox' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', ', ' );
-
-			if ( ! toolbox_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'toolbox' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'toolbox' );
-				}
-
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'toolbox' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'toolbox' );
-				}
-
-			} // end check for categories on this blog
-		?>
-		<div>
-            <a href="<?php echo get_previous_posts_link(); ?>">&larr;</a>
-            <?php echo the_date(); ?>
-            <a href="<?php echo get_next_posts_link(); ?>">&rarr;</a>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    
+        <div id="text" class="text-container">
+            <?php the_content(); ?>
         </div>
-	</footer><!-- .entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+    
+    </article><!-- #post-<?php the_ID(); ?> -->
+
+    <div class="post-footer">
+        <div class="post-nav">
+            <?php previous_post_link('%link', '&larr;', TRUE); ?>
+            <span class="post-date">
+                <?php the_date(); ?>
+            </span>
+            <?php next_post_link('%link', '&rarr;', TRUE); ?>
+        </div>
+    
+        <?php
+            /* translators: used between list items, there is a space after the comma */
+            $tags_list = get_the_tag_list( '', __( ', ', 'toolbox' ) );
+            if ( $tags_list ) :
+        ?>
+            <div class="post-tags">
+                <?php printf( __( 'Tags: %1$s', 'toolbox' ), $tags_list ); ?>
+            </div>
+        <?php endif; // End if $tags_list ?>
+    </div>
+</section>
