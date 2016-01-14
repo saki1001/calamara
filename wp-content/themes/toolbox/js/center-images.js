9bf5b6e430ac051da46fcf1b9e6434175e820ed5
Saki Sato <saki.s.sato@gmail.com>
2016-01-14 14:22:34 -0800

    fixing slideshow center position after update

--- wp-content/themes/toolbox/js/center-images.js
+++ wp-content/themes/toolbox/js/center-images.js
@@ -1,21 +1,26 @@
 var $j = jQuery.noConflict();
 
-$j(document).ready(function() {
+$j(window).on('load', function() {
 
     // Set Image Width and Margin
     // for centering images in portfolio pages
     $j('#media .image-container figure').each(function(i) {
         
-        var width = $j(this).children('img').width();
-        var height = $j(this).children('img').height();
+        var img = $j(this).children('img');
+        var width = img.width();
+        var height = img.height();
         var marginTop = (500 - height)/2 + 'px';
         
+        // If image shorter than container
         // add marginTop and negative marginTop to bottom
         // to prevent container from becoming too large
-        $j(this).css({
-            'width': width,
-            'margin': marginTop + ' auto -' + marginTop
-        });
+        if( height < img.parent().outerHeight() ) {
+
+            img.css({
+                'width': width,
+                'margin': marginTop + ' auto -' + marginTop
+            });
+        }
     });
 
 });
