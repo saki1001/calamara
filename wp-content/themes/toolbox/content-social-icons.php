5befb7b4d8f74828371c397229c6bb240c5ed035
Saki Sato <sakiisato@gmail.com>
2016-01-18 17:32:52 -0500

    adding google plus icon

--- wp-content/themes/toolbox/content-social-icons.php
+++ wp-content/themes/toolbox/content-social-icons.php
@@ -15,6 +15,7 @@
     $image = urlencode(get_thumbnail_custom($post->ID, 'post-thumbnail'));
 ?>
 <ul class="social-icons">
+    <li>Share with:</li>
     <li>
         <a onClick="window.open('http://pinterest.com/pin/create/button/?url=<?php echo $url; ?>&amp;media=<?php echo $image; ?>&amp;description=<?php echo $desc; ?>','sharer','toolbar=0,status=0,width=650,height=225');" href="javascript: void(0)" class="pintrest" title="Pin It" target="_blank"></a>
     </li>
@@ -23,5 +24,8 @@
     </li>
     <li>
         <a onClick="window.open('http://www.facebook.com/sharer.php?s=100&amp;p[title]=<?php echo $title . ' | Mara G. Haseltine'; ?>&amp;p[summary]=<?php echo $desc + ' @MaraTheGreat';?>&amp;p[url]=<?php echo $url; ?>&amp;&amp;p[images][0]=<?php echo $image;?>','sharer','toolbar=0,status=0,width=548,height=325');" href="javascript: void(0)" class="facebook" title="Share on Facebook." target="_blank"></a>
+    </li>
+    <li>
+        <a href="https://plus.google.com/share?url={<?php echo $url; ?>}" onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;" href="javascript: void(0)" class="google" title="Share on Google Plus." target="_blank"></a>
     </li>
 </ul>
