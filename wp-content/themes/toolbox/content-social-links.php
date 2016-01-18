d1c8e9f86cca5b8f1bc2c1436a856a56f142010c
Saki Sato <sakiisato@gmail.com>
2016-01-18 17:33:18 -0500

    adding maras social media account links to footer

---
+++ wp-content/themes/toolbox/content-social-links.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template used for displaying social media icons
+ *
+ * @package Toolbox
+ * @since Toolbox 1.0
+ */
+?>
+
+<ul class="social-icons">
+    <li>
+        <a href="https://www.pinterest.com/calamara0179/" class="pintrest" title="Mara's Pinterest" target="_blank"></a>
+    </li>
+    <li>
+        <a href="https://twitter.com/maraghaseltine/" class="twitter" title="Mara's Twitter" target="_blank"></a>
+    </li>
+    <li>
+        <a href="https://www.facebook.com/MaraTheGreat/" class="facebook" title="Mara's Facebook" target="_blank"></a>
+    </li>
+    <li>
+        <a href="https://www.instagram.com/maraghaseltine/" class="instagram" title="Mara's Instagram" target="_blank"></a>
+    </li>
+    <li>
+        <a href="https://plus.google.com/115515563323491366007/" class="google" title="Mara's Google Plus." target="_blank"></a>
+    </li>
+</ul>
